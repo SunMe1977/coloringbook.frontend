4390be175600ac9765c3276da3020398821629f8
hjh <hansjoerghofer692@gmail.com>
2025-09-07 10:17:11 +0200

    reduce image

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,7 +1,25 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    open: true,
+    fs: {
+      strict: false,
+    },
+  },
+  base: '/',
 });
