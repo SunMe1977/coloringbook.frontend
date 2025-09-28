4c42d7ff7327bb9adebc62e16bd52ff6711b8f3e
hjh <hansjoerghofer692@gmail.com>
2025-09-28 18:29:04 +0200

    fix service worker

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import fundraisingAI from '../img/fundraisingAI.png'; // Import the new image
+import fundraisingAI from '../img/fundraisingAI.jpg';
 
 const Home: React.FC = () => {
   return (
